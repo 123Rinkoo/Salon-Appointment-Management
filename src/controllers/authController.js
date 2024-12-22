1f6973c9a2a029050860e4606209ce24b4593db3
123Rinkoo <rinkookandpal1966@gmail.com>
2024-12-22 18:37:55 +0530

    Implement login API with PASETO token generation

---
+++ src/controllers/authController.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const { generateToken } = require('../utils/paseto');
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashedPassword, role });
+  await user.save();
+  res.status(201).json({ message: 'User registered successfully' });
+};
+
