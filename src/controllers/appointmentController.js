3227dbe9445b33d23d1b59eb7c9f8081fc313c78
123Rinkoo <rinkookandpal1966@gmail.com>
2024-12-25 12:04:35 +0530

    Implement get appointment by ID API

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,11 +1,12 @@
 const { validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 const Appointment = require('../models/Appointment');
 const Service = require('../models/Service');
 
 exports.createAppointment = async (req, res) => {
     try {
         const errors = validationResult(req);
-        
+
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
@@ -61,5 +62,42 @@ exports.createAppointment = async (req, res) => {
         }
 
         res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+};
+
+exports.getAppointmentById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid appointment ID' });
+        }
+
+        const appointment = await Appointment.findById(id)
+            .populate('userId', 'name email')
+            .populate('serviceId', 'name price')
+            .select('-__v') 
+            .exec();
+
+        if (!appointment) {
+            return res.status(404).json({ message: 'Appointment not found' });
+        }
+
+        const isAdmin = req.user.role === 'Admin';
+        const isOwner = req.user.id === appointment.userId._id.toString();
+      
+        if (!isAdmin && !isOwner) {
+            return res.status(403).json({ message: 'Access denied' });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: appointment,
+        });
+    } catch (error) {
+        console.error('Error fetching appointment:', error);
+        res.status(500).json({
+            success: false,
+            message: 'An unexpected error occurred. Please try again later.',
+        });
     }
 };
--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createAppointment } = require('../controllers/appointmentController');
+const { createAppointment , getAppointmentById} = require('../controllers/appointmentController');
 const authenticate = require('../middlewares/authMiddleware.js');
 const rbacMiddleware = require('../middlewares/rbacMiddleware');
 const router = express.Router();
@@ -8,5 +8,7 @@ router.use(authenticate); // This will verify the token for all routes below
 
 // Create a new appointment 
 router.post('/', rbacMiddleware(['Customer']), createAppointment);
+
+router.get('/:id', rbacMiddleware(['Admin', 'Staff', 'Customer']), getAppointmentById);
 
 module.exports = router;
